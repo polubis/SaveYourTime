8d0d2e5d04668243609ae312e431ec84add5e1ae
polubis <polubis@wp.pl>
2018-12-17 11:04:43 +0100

    Add meals component

--- save-time-app/src/app/containers/main/main-routing.module.ts
+++ save-time-app/src/app/containers/main/main-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { MainComponent } from "src/app/containers/main/main.component";
 import { ProductsComponent } from "src/app/containers/main/products/products.component";
 import { DashboardComponent } from "src/app/containers/main/dashboard/dashboard.component";
+import { MealsComponent } from "src/app/containers/main/meals/meals.component";
 
 const routes: Routes = [
     { path: '', component: MainComponent, children: [
         { path: '', component: DashboardComponent },
-        { path: 'products', component: ProductsComponent }
+        { path: 'products', component: ProductsComponent },
+        { path: 'meals', component: MealsComponent },
       ]
     }
 ];
--- save-time-app/src/app/containers/main/main.module.ts
+++ save-time-app/src/app/containers/main/main.module.ts
@@ -6,9 +6,10 @@ import { MainRoutingModule } from "src/app/containers/main/main-routing.module";
 import { UtilsModule } from "src/app/components/utils/utils.module";
 import { ProductsComponent } from "src/app/containers/main/products/products.component";
 import { DashboardComponent } from "src/app/containers/main/dashboard/dashboard.component";
+import { MealsComponent } from './meals/meals.component';
 
 @NgModule({
-  declarations: [ MainComponent, DashboardComponent, ProductsComponent ],
+  declarations: [ MainComponent, DashboardComponent, ProductsComponent, MealsComponent ],
   imports: [ MainRoutingModule, CommonModule, UtilsModule ]
 })
 export class MainModule {
