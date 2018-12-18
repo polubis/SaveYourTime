425a56953cb1bfdec1fa60b657ff1cf33625783e
polubis <polubis@wp.pl>
2018-12-18 23:55:04 +0100

    file-picker fixes

--- save-time-app/src/app/components/utils/file-picker/file-picker.component.ts
+++ save-time-app/src/app/components/utils/file-picker/file-picker.component.ts
@@ -11,7 +11,16 @@ export class FilePickerComponent extends InputBase {
   @Input() classes = '';
   @Input() icon = 'add_a_photo';
   @Input() path: string;
+  deleteOption = true;
   constructor() {
     super(new Setting('product picture', { isPicture: true, isFileWithCorrectSize: 300000 }, '', 'file'));
+  }
+
+  removeDeleteOption() {
+    this.deleteOption = false;
+  }
+
+  enableDeleteOption() {
+    this.deleteOption = true;
   }
 }
